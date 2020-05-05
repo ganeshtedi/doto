92ce73fcf404f69152b40b77394a54b120450bfe
ganeshtedi <baisaniganesh@gmail.com>
2020-05-06 00:16:05 +0530

    first commit

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.use(express.static(__dirname + '/public/css'));
 app.use(express.static(__dirname + '/public/images'));
 app.get('/',(req,res)=>{
     console.log('rendered index');
-   res.render('index');
+    res.sendFile(path.join(__dirname+'/index.html'));
+   //res.render('index');
   // res.sendFile('index.html');
    console.log('okay with this index');
 });
